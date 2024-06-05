2e7af3c1de68abc0463ba0013126760c021cba84
dat <tranminhdat0805@gmail.com>
2024-06-06 00:27:26 +0700

    Add sidebar layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,35 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Auth/Login";
 import SignUp from "./pages/Auth/Signup";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Layout from "./layout/Layout";
+import Package from "./pages/Package/Package";
+import NotFound from "./pages/404/404";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route element={<AdminLayout />}> */}
         <Route path="/" element={<Home />} />
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/signup" element={<SignUp />} />
-        {/* </Route> */}
+        <Route
+          path="/dashboard"
+          element={
+            <Layout>
+              <Dashboard />
+            </Layout>
+          }
+        />
+        <Route
+          path="/package"
+          element={
+            <Layout>
+              <Package />
+            </Layout>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,5 +1,52 @@
-const Layout = () => {
-  return <>Layout</>;
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { MdSpaceDashboard } from "react-icons/md";
+import { GoPackage } from "react-icons/go";
+import { NavLink, useLocation } from "react-router-dom";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
+  const location = useLocation();
+  const activeTab = location.pathname.split("/")[1];
+
+  return (
+    <ResizablePanelGroup direction="horizontal" className="h-screen w-screen">
+      <ResizablePanel
+        defaultSize={15}
+        minSize={10}
+        className="flex flex-col justify-start bg-slate-700 py-3 select-none"
+      >
+        <NavLink
+          to="/dashboard"
+          className={`text-white w-full px-4 text-center my-1 text-xs mt-3 hover:bg-slate-500  flex flex-row justify-start items-center ${
+            activeTab === "dashboard" ? "bg-slate-500" : ""
+          }`}
+        >
+          <MdSpaceDashboard size={25} />
+          <p className="text-sm p-2 ">Bảng điều khiển</p>
+        </NavLink>
+        <NavLink
+          to="/package"
+          className={`text-white w-full px-4 text-center my-1 text-xs mt-3 hover:bg-slate-500  flex flex-row justify-start items-center ${
+            activeTab === "package" ? "bg-slate-500" : ""
+          }`}
+        >
+          <GoPackage size={25} />
+          <p className="text-sm p-2 ">Gói</p>
+        </NavLink>
+      </ResizablePanel>
+      <ResizableHandle withHandle className="h-screen" />
+      <ResizablePanel defaultSize={85} minSize={50} className="p-3">
+        {children}
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
 };
 
 export default Layout;
