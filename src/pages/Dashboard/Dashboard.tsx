60ca423a1e24c568f6b7fbbd20cdabb6c6b7d347
Khaintse161879 <khai14tv@gmail.com>
2024-06-21 22:01:59 +0700

    changeicon & Pagination

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,64 +1,29 @@
-"use client";
-import CardDataStats from "@/components/CardDataStats";
-import Chart from "@/components/Chart";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { authAxios } from "@/services/axios";
-import React, { useEffect, useState } from "react";
+import CardDataStats from '@/components/CardDataStats';
+import Chart from '@/components/Chart';
+import { Pagination, PaginationContent, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from '@/components/ui/pagination';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { authAxios } from '@/services/axios';
+import React, { useEffect, useState } from 'react';
+import { FaCashRegister } from 'react-icons/fa';
+import { RiUserFill, RiUserFollowFill } from 'react-icons/ri';
 
-import { IoIosTrendingUp, IoLogoDropbox, IoMdPerson } from "react-icons/io";
-import DashboardProps from "./DashboardProps";
-
-// const invoices = [
-//   {
-//     invoice: "INV001",
-//     paymentStatus: "Paid",
-//     totalAmount: "250.00",
-//     paymentMethod: "Credit Card",
-//   },
-//   {
-//     invoice: "INV002",
-//     paymentStatus: "Pending",
-//     totalAmount: "$150.00",
-//     paymentMethod: "PayPal",
-//   },
-//   {
-//     invoice: "INV003",
-//     paymentStatus: "Unpaid",
-//     totalAmount: "$350.00",
-//     paymentMethod: "Bank Transfer",
-//   },
-//   {
-//     invoice: "INV004",
-//     paymentStatus: "Paid",
-//     totalAmount: "$450.00",
-//     paymentMethod: "Credit Card",
-//   },
-//   {
-//     invoice: "INV005",
-//     paymentStatus: "Paid",
-//     totalAmount: "$550.00",
-//     paymentMethod: "PayPal",
-//   },
-//   {
-//     invoice: "INV006",
-//     paymentStatus: "Pending",
-//     totalAmount: "$200.00",
-//     paymentMethod: "Bank Transfer",
-//   },
-//   {
-//     invoice: "INV007",
-//     paymentStatus: "Unpaid",
-//     totalAmount: "$300.00",
-//     paymentMethod: "Credit Card",
-//   },
-// ];
+interface DashboardProps {
+  totalUser: number;
+  revenue: number;
+  payment: Array<{
+    _id: string;
+    orderCode: string;
+    status: boolean;
+    userId: {
+      username: string;
+    };
+    package_id: {
+      packageName: string;
+    };
+    amount: number;
+  }>;
+  registeredUser: number;
+}
 
 const Dashboard: React.FC = () => {
   const [dashboard, setDashboard] = useState<DashboardProps>({
@@ -68,51 +33,45 @@ const Dashboard: React.FC = () => {
     registeredUser: 0,
   });
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  useEffect(() => {
+    getDashboard();
+  }, []);
+
   const getDashboard = async () => {
     try {
-      const response = await authAxios.get("/dashboard");
-      if (response.data.message === "success") {
-        const { totalUser, revenue, payment, registeredUser } =
-          response.data.data;
+      const response = await authAxios.get('/dashboard');
+      if (response.data.message === 'success') {
+        const { totalUser, revenue, payment, registeredUser } = response.data.data;
         console.log(totalUser, revenue, payment, registeredUser);
         setDashboard({ totalUser, revenue, payment, registeredUser });
+        setTotalPages(Math.ceil(payment.length / 10));
       } else {
+
       }
     } catch (error) {
       console.log(error);
     }
   };
-  useEffect(() => {
-    getDashboard();
-  }, []);
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const paginatedData = dashboard.payment?.slice((currentPage - 1) * 10, currentPage * 10);
+
   return (
     <>
       <div className="grid grid-cols-1 gap-3 md:grid-cols-2 md:gap-6 xl:grid-cols-3 2xl:gap-7.5">
-        {/* <div>
-          <CardDataStats
-            title="Total views"
-            total="3.456K"
-            currency="VND"
-            levelUp
-          >
-            <IoIosEye
-              className="size-10 fill-primary dark:fill-white"
-              width="100"
-              height="100"
-            />
-          </CardDataStats>
-        </div> */}
         <CardDataStats
           title="Doanh thu"
           total={dashboard.revenue.toString()}
           currency="VND"
           levelUp
         >
-          <IoLogoDropbox
-            className="size-10 fill-primary dark:fill-white"
-            width="100"
-            height="100"
-          />
+          <FaCashRegister className="size-10 fill-primary dark:fill-white" width="100" height="100" />
         </CardDataStats>
         <CardDataStats
           title="Tổng người dùng"
@@ -120,11 +79,7 @@ const Dashboard: React.FC = () => {
           currency=""
           levelUp
         >
-          <IoMdPerson
-            className="size-10 fill-primary dark:fill-white"
-            width="100"
-            height="100"
-          />
+          <RiUserFill className="size-10 fill-primary dark:fill-white" width="100" height="100" />
         </CardDataStats>
         <CardDataStats
           title="Đã đăng ký"
@@ -132,29 +87,22 @@ const Dashboard: React.FC = () => {
           currency=""
           levelUp
         >
-          <IoIosTrendingUp
-            className="size-10 fill-primary dark:fill-white"
-            width="100"
-            height="100"
-          />
+          <RiUserFollowFill className="size-10 fill-primary dark:fill-white" width="100" height="100" />
         </CardDataStats>
       </div>
 
-      <div className="flex-auto flex-col ">
+      <div className="flex-auto flex-col">
         <div className="whitespace-nowrap font-semibold text-lg mt-5 mb-5">
           <Chart />
         </div>
       </div>
 
-      <div className="flex-auto flex-col ">
+      <div className="flex-auto flex-col">
         <div className="whitespace-nowrap font-semibold text-lg mt-5 mb-5">
-          {" "}
-          {/* Remove TableCaption and use a div with the appropriate classes */}
           Lịch sử giao dịch
         </div>
         <div className="w-full bg-white mt-3 rounded-lg">
           <Table>
-            {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
             <TableHeader>
               <TableRow>
                 <TableHead>Mã order</TableHead>
@@ -165,27 +113,46 @@ const Dashboard: React.FC = () => {
               </TableRow>
             </TableHeader>
             <TableBody>
-              {dashboard.payment?.map((invoice) => (
+              {paginatedData?.map((invoice) => (
                 <TableRow key={invoice._id}>
-                  <TableCell className="font-medium">
-                    {invoice.orderCode}
-                  </TableCell>
-                  <TableCell>
-                    {invoice.status ? "Đã thanh toán" : "Đang chờ thanh toán"}
-                  </TableCell>
+                  <TableCell className="font-medium">{invoice.orderCode}</TableCell>
+                  <TableCell>{invoice.status ? 'Đã thanh toán' : 'Đang chờ thanh toán'}</TableCell>
                   <TableCell>{invoice.userId.username}</TableCell>
                   <TableCell>{invoice.package_id.packageName}</TableCell>
                   <TableCell>{invoice.amount}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
-            {/* <TableFooter>
-              <TableRow>
-                <TableCell colSpan={3}>Total</TableCell>
-                <TableCell className="text-right">$2,500.00</TableCell>
-              </TableRow>
-            </TableFooter> */}
           </Table>
+          <Pagination>
+            <PaginationContent>
+              <PaginationItem>
+                <PaginationPrevious
+                  href="#"
+                  onClick={() => handlePageChange(currentPage - 1)}
+                // disabled={currentPage === 1}
+                />
+              </PaginationItem>
+              {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+                <PaginationItem key={page}>
+                  <PaginationLink
+                    href="#"
+                    onClick={() => handlePageChange(page)}
+                  // active={page === currentPage}
+                  >
+                    {page}
+                  </PaginationLink>
+                </PaginationItem>
+              ))}
+              <PaginationItem>
+                <PaginationNext
+                  href="#"
+                  onClick={() => handlePageChange(currentPage + 1)}
+                // disabled={currentPage === totalPages}
+                />
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
         </div>
       </div>
     </>
